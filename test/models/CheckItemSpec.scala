55ed160a9b7073c110bcadd93d111586dc52cae4
JamesMMiller <7511223+JamesMMiller@users.noreply.github.com>
2025-11-23 17:33:56 +0000

    fix: Update CheckItemSpec to remove references to moved items

--- test/models/CheckItemSpec.scala
+++ test/models/CheckItemSpec.scala
@@ -23,37 +23,5 @@ class CheckItemSpec extends AnyWordSpec with Matchers {
       checkItem.id shouldBe "2.B"
       checkItem.description shouldBe "Security check"
     }
-
-    "have predefined items" in {
-      CheckItem.items should not be empty
-      CheckItem.items.length shouldBe 4
-    }
-
-    "have items with valid IDs" in {
-      CheckItem.items.foreach { item =>
-        item.id should not be empty
-        item.id should fullyMatch regex "[0-9]+\\.[A-Z]+"
-      }
-    }
-
-    "have items with descriptions" in {
-      CheckItem.items.foreach { item =>
-        item.description should not be empty
-        item.description.length should be > 10
-      }
-    }
-
-    "contain expected check items" in {
-      val ids = CheckItem.items.map(_.id)
-      ids should contain("1.A")
-      ids should contain("1.C")
-      ids should contain("2.A")
-      ids should contain("4.B")
-    }
-
-    "have unique IDs" in {
-      val ids = CheckItem.items.map(_.id)
-      ids.distinct.length shouldBe ids.length
-    }
   }
 }
